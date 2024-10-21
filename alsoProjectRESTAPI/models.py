2e94171d09d1127715c5dae96b52902efc723825
niko-niko-nom <109971471+niko-niko-nom@users.noreply.github.com>
2024-10-21 14:37:56 +0200

    Add ToDoItem model and views

--- alsoProjectRESTAPI/models.py
+++ alsoProjectRESTAPI/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class ToDoItem(models.Model):
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=250)
+    description = models.TextField()
+    createdBy = models.CharField(max_length=250)
+    status = models.CharField(max_length=50)
+    dateCreated = models.DateTimeField(auto_now_add=True)
+    dateUpdated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
--- projectRESTAPI/settings.py
+++ projectRESTAPI/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'alsoProjectRESTAPI',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
