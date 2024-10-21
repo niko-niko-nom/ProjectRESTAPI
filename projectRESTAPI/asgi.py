da30a21cce5440381525b500afaf4bc6f537c63d
niko-niko-nom <109971471+niko-niko-nom@users.noreply.github.com>
2024-10-21 14:09:18 +0200

    Project restart

--- projectRESTAPI/asgi.py
+++ projectRESTAPI/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for djangroRESTproject project.
+ASGI config for projectRESTAPI project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangroRESTproject.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'projectRESTAPI.settings')
 
 application = get_asgi_application()
